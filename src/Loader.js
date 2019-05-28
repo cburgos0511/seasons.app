00c9eaec0abe75f85e991bb48fa57ef6e7ac2ce2
Cruz Burgos <cburgos@Christines-MacBook-Pro.local>
2019-05-28 15:56:05 -0500

    Initial commit

---
+++ src/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Loader = (props) => {
+    return (
+        <div class="ui active dimmer">
+            <div class="ui massive text loader">
+                {props.message}
+            </div>
+        </div>
+    );
+};
+
+Loader.defaultProps = {
+    message: 'Loading...'
+};
+
+export default Loader; 
